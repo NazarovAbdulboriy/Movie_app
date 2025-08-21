08964f8585c2c16a2b063f005894168ac59c5352
NazarovAbdulboriy <158128955+NazarovAbdulboriy@users.noreply.github.com>
2025-08-21 16:17:52 +0500

    commits

--- src/features/not-found/pages/Not-found.tsx
+++ src/features/not-found/pages/Not-found.tsx
@@ -5,13 +5,13 @@ const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-900 text-white">
+    <div className="flex flex-col items-center justify-center h-screen bg-black text-white">
       <h1 className="text-6xl font-bold mb-4">404</h1>
       <p className="text-xl mb-6">Oops! Page not found.</p>
       <div className="flex gap-4">
         <Link
           to="/"
-          className="px-6 py-3 bg-blue-600 rounded-xl shadow-lg hover:bg-blue-700 transition"
+          className="px-6 py-3 bg-red-500 rounded-xl shadow-lg hover:bg-red-600 transition"
         >
           Go Home
         </Link>
